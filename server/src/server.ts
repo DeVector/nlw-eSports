8faa9b1be33268f9e63cb2a463cfdf25edc7b0b1
DeVector <diniz.vito@gmail.com>
2022-09-15 09:34:12 -0300

    Create methods to games and ads

---
+++ server/src/server.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+
+const app = express();
+
+/** Retorna todos os games */
+app.get('/games', (request, response) => {
+
+    return response.json();
+
+});
+
+/** Retorna os Ads de cada game */
+app.get('/games/:id/ads', (request, response) => {
+
+});
+
+/* Retorna o nome do discord */
+app.get('ads/:id/discord', (request, response) => {
+    
+});
+
+/** Criando um Ad */
+app.post('games/:id/ad', (request, response) => {
+
+    /** Pegando o id informado no corpo da requisição */
+    const gameId = request.params.id;
+
+    return response.json();
+
+});
+
+app.listen(3333);
